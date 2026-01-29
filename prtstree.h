6aac4cd2a6382df8208a3b62a4a3468a786bf92e
gaperez64 <gaperez64@gmail.com>
2026-01-29 09:38:42 +0100

    added an include guard for the header

--- prtstree.h
+++ prtstree.h
@@ -1,5 +1,11 @@
+#ifndef PRTSTREE_H
+#define PRTSTREE_H 1
+
 enum : char { ONE = '1', ZERO = '0', EPSILON = 'e', COMMA = ',', EOS = '|' };
 
 void print_tree_dot(unsigned const nlabs, char const labels[nlabs]);
 
-void print_tree_ppart(unsigned const nlabs, char const labels[nlabs], int const p);
+void print_tree_ppart(unsigned const nlabs, char const labels[nlabs],
+                      int const p);
+
+#endif
